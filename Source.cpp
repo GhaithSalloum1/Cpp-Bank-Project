69f372bff2c793307c47a5cc21b5e754d28b912e
Ghaith <ghaithsalloum12@gmail.com>
2025-09-30 01:33:46 +0300

    Main

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,38 @@
-#include"clsBankClient.h"
+
+#include <iostream>
+#include "clsBankClient.h"
+
+int main()
+
+{
+
+    clsBankClient Client1 = clsBankClient::Find("A101");
+    if (!Client1.IsEmpty())
+    {
+        cout << "\nClient Found :-)\n";
+    }
+    else
+    {
+        cout << "\nClient Was not Found :-(\n";
+    }
+
+    Client1.Print();
+
+    clsBankClient Client2 = clsBankClient::Find("A101", "1234");
+    if (!Client2.IsEmpty())
+    {
+        cout << "\nClient Found :-)\n";
+    }
+    else
+    {
+        cout << "\nClient Was not Found :-(\n";
+    }
+    Client2.Print();
 
 
+    cout << "\nIs Client Exist? " << clsBankClient::IsClientExist("A101");
 
 
-
-int main() {
-
-
-
-
+    system("pause>0");
+    return 0;
 }
